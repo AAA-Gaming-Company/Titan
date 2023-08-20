3d7ece0a2f94e9814b707af4ee5fc93c2b1d73ed
ImDaBigBoss <67973871+ImDaBigBoss@users.noreply.github.com>
2023-08-20 14:51:05 +0200

    Added Player controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(SpriteRenderer))]
+public class PlayerController : MonoBehaviour {
+    public float playerSpeed;
+
+    private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
+
+    private void Start() {
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update() {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        if (x != 0 || y != 0) {
+            //TODO: Add move sound
+        }
+
+        UpdatePlayerSprite(x, y);
+        rb.AddForce(new Vector2(x * this.playerSpeed, y * this.playerSpeed));
+
+        if (Input.GetMouseButtonDown(0)) {
+            this.Hit();
+
+            //Probably should also rotate the player if they click in a
+            // different direction to that in which they are going.
+        }
+    }
+
+    private void UpdatePlayerSprite(float x, float y) {
+        //TODO: Update sprite
+    }
+
+    private void Hit() {
+        //TODO: Add gunz
+    }
+}
