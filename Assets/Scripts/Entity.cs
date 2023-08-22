4270650040bdf97955905a30b7eca261e6250aa0
Charles <charlesglucas@gmail.com>
2023-08-22 11:19:51 +0200

    Feedback & stuff

--- Assets/Scripts/Entity.cs
+++ Assets/Scripts/Entity.cs
@@ -1,10 +1,14 @@
 using UnityEngine;
+using MoreMountains.Feedbacks;
 
 public abstract class Entity : MonoBehaviour {
     [Header("Entity")]
     public int maxHP;
 
-    private int currentHP;
+    public int currentHP;
+
+    public MMF_Player damageFeedback;
+    public MMF_Player deathFeedback;
 
     protected void Start() {
         this.currentHP = this.maxHP;
@@ -15,12 +19,15 @@ public abstract class Entity : MonoBehaviour {
 
     public void TakeDamage(int amount) {
         this.currentHP -= amount;
+        damageFeedback.PlayFeedbacks();
         if (this.currentHP <= 0) {
             this.Die();
         }
     }
 
     public void Die() {
+        deathFeedback.transform.parent = null;
+        deathFeedback.PlayFeedbacks();
         Destroy(this.gameObject);
     }
 }
--- Assets/Scripts/World/TriggerArea.cs
+++ Assets/Scripts/World/TriggerArea.cs
@@ -1,5 +1,12 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class TriggerArea : MonoBehaviour {
+    private GameObject player;
+    public UnityEvent triggerAction;
 
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        triggerAction.Invoke();
+    }
 }
