dcd7aebe248b4bd435d70d7f6cb3780909044781
ImDaBigBoss <67973871+ImDaBigBoss@users.noreply.github.com>
2023-08-25 20:34:13 +0200

    Added tutorial screens with more info

---
+++ Assets/Scripts/World/Triggers/TutorialTriggerArea.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialTriggerArea : TriggerArea {
+    [Header("Tutorial")]
+    public GameObject tutoScreen;
+    public Button closeButton;
+
+    private void Start() {
+        this.tutoScreen.SetActive(false);
+        this.closeButton.onClick.AddListener(this.CloseButton);
+        this.destroyOnTrigger = false;
+    }
+
+    protected override void TriggerAction() {
+        this.tutoScreen.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    private void CloseButton() {
+        Time.timeScale = 1f;
+        Destroy(this.tutoScreen);
+        Destroy(this.gameObject);
+    }
+}
